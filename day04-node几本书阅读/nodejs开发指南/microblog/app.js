0c241d71afc566a64d9a3f2dd0a4ebbd7a827698
EricLee <179310009@qq.com>
2017-09-21 17:37:08 +0800

    001

--- day04-node几本书阅读/nodejs开发指南/microblog/app.js
+++ day04-node几本书阅读/nodejs开发指南/microblog/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var routers = require('./routes/router');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', routers.showIndex);
+app.get('/hello', routers.showHello);
+app.get('/users/:username', routers.showUsers);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
---
+++ day04-node几本书阅读/nodejs开发指南/microblog/routes/router.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.showIndex = function (req,res,next) {
+    res.json({title: 'Express'});
+};
+router.showHello = function (req,res,next) {
+    res.send('The time is ' + new Date().toString());
+};
+
+router.showUsers = function (req,res,next) {
+    res.send('user:'+req.params.username);
+};
+module.exports = router;
