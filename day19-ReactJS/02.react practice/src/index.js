3030c88aba4a7978d1416f558086edecc22fea32
Eric Lee <179310009@qq.com>
2018-07-18 15:15:28 +0800

    webpack

--- day19-ReactJS/02.react practice/src/index.js
+++ day19-ReactJS/02.react practice/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(<div>
     1234
-</div>,document.getElementById('app'));http://10.160.237.83/Eric/mapof12345.git
+</div>,document.getElementById('app'));
