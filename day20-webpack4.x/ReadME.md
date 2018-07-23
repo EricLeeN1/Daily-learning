4a1f59a11207d0aa2cba6b1a36b3a7d7ca85be57
Eric Lee <179310009@qq.com>
2018-07-23 08:05:28 +0800

    webpack

--- day20-webpack4.x/ReadME.md
+++ day20-webpack4.x/ReadME.md
@@ -1,10 +1,11 @@
 #webpack4.学习
 
-##一、全局安装webpack及webpack-cli
-npm install webpack webpack-cli -g
-##二、本地安装
-npm install webpack -D
-##三、创建基本的webpack4.x项目
+##一、 前置条件
+	- **全局安装**
+		webpack及webpack-cli -> npm install webpack webpack-cli -g
+	- **本地安装**(需要不同版本需求时)
+		npm install webpack -D
+##二、创建基本的webpack4.x项目
 1. 运行`npm init -y`快速初始化项目
 2. 在项目根目录创建`src`源代码目录和`dist`发布目录
 3. 在src目录下创建`index.html`
@@ -14,16 +15,107 @@ npm install webpack -D
 	- 默认约定了
 		- 打包的入口是`src`->`index.js`
 		- 打包的输出文件是`dist`->`main.js`
-		- 4.x中新增了`mode`选项，可选的值为：`development`、`production`
-
-	- `"dev": "webpack-dev-server --open firefox --port 3000  --hot --progress --compress"`
-		- `--open firefox：`设置自动打开浏览器，如果不加参数默认浏览器打开。
+	- 4.x中新增了`mode`选项，可选的值为：`development`、`production`
+	- **scripts中dev设置：**
+		
+		`"dev": "webpack-dev-server --open firefox --port 3000  --hot --progress --compress"`
+		- `--open firefox：`设置自动打开浏览器为火狐，如果不加参数默认浏览器打开。
 		- `--port 3000`设置端口号为3000
 		- `--compress`设置压缩
 		- `--hot`设置热替换
 		- `--progress`设置显示进度
 
-##四、webpack中使用的包
+##三、入口
+	
+webpack 会读取这个文件，并从它开始解析依赖，然后进行打包。
+
+		- 默认约定了
+		- 打包的入口是`src`->`index.js`
+		module.exports = {
+  			entry: './src/index.js' 
+		}
+	
+##四、loader
+	
+模块代码转换 -> 我们可以在`module.rules`字段下来配置相关的规则。
+	
+1.示例代码：
+
+
+	`rules: [
+    {
+      test: /\.jsx?/, // 匹配文件路径的正则表达式，通常我们都是匹配文件类型后缀
+      include: [
+        path.resolve(__dirname, 'src') // 指定哪些路径下的文件需要经过 loader 处理
+      ],
+      use: 'babel-loader', // 指定使用的 loader
+    },
+  	],`
+  	
+2. **配置loader**
+3. **规则条件配置**
+
+	- { test: ... } 匹配特定条件
+	- { include: ... } 匹配特定路径
+	- { exclude: ... } 排除特定路径
+	- { and: [...] }必须匹配数组中所有条件
+	- { or: [...] } 匹配数组中任意一个条件
+	- { not: [...] } 排除匹配数组中所有条件
+	
+4. 执行顺序
+
+		前置 enforce: 'pre' // 指定为前置类型
+		行内 enforce：'post' // 指定为后置类型
+		普通
+		后置
+		
+5. 使用noParse
+	对于一些不需要解析依赖（即无依赖） 的第三方大型类库等，可以通过这个字段来配置，以提高整体的构建速度。
+  		
+  		`noParse: /jquery|lodash/, // 正则表达式
+    	// 或者使用 function
+    	noParse(content) {
+      		return /jquery|lodash/.test(content)
+    	}`
+    	
+##五、plugin
+
+用于处理更多其他的一些构建任务提升能力
+
+1. 压缩JS代码的 uglifyjs-webpack-plugin 插件；
+
+		`const UglifyPlugin = require('uglifyjs-webpack-plugin')
+		module.exports = {
+  			plugins: [
+    			new UglifyPlugin()
+  			],
+		}`
+		
+2. 定义环境变量 DefinePlugin 插件；
+3. 生成CSS文件的 ExtractTextWebpackPlugin 等；
+4. **操作流程：**
+	1. 引包 `const UglifyPlugin = require('uglifyjs-webpack-plugin')；
+	2. 添加到插件数组中
+5. 常用插件功能及使用
+	1. **DefinePlugin**：DefinePlugin 是 webpack 内置的插件，可以使用 webpack.DefinePlugin 直接获取。
+		1. 用于创建一些在编译时可以配置的全局常量，这些常量的值我们可以在 webpack 的配置中去指定，
+
+##六、输出
+
+输出即指 webpack 最终构建出来的静态文件，用 `output` 字段。
+默认创建的输出内容就是 ./dist/main.js。
+
+
+	`// 路径中使用 hash，每次构建时会有一个不同 hash 值，避免发布新版本时线上使用浏览器缓存
+	module.exports = {
+  		output: {
+    		filename: '[name].js',
+    		path: __dirname + '/dist/[hash]',
+  		},
+	}`
+
+
+#最后、webpack中使用的包
 
 	`-D` -> 工具
 	`-S` -> 开发到上线都要使用的包
@@ -32,6 +124,7 @@ npm install webpack -D
 	webpack-cli -> webpack脚手架
 	webpack-dev-server -> webpack服务
 	html-webpack-plugin -> html打包
+	extract-text-webpack-plugin -> 导出CSS文件，请安装最新版 -> npm install extract-text-webpack-plugin@next -D
 	babel-core
 	babel-loader
 	babel-plugin-transform-runtime
@@ -40,11 +133,12 @@ npm install webpack -D
 	babel-preset-stage-0
 	react -> 专门用于创建组件，同时组件的生命周期都在这个包中
 	react-dom -> 专门进行DOM操作的，最主要的应用场景就是`ReactDOM.render()`
-	css-loader
-	style-loader
+	css-loader -> css-loader 负责解析 CSS 代码，主要是为了处理 CSS 中的依赖，例如 @import 和 url() 等引用外部文件的声明；
+	style-loader -> style-loader 会将 css-loader 解析的结果转变成 JS 代码，运行时动态插入 style 标签来让 CSS 代码生效。
 	url-loader -> 用来处理文字，图片
-	file-loader -> url-loader依赖项之一
+	file-loader -> 直接输出文件，把构建后的文件路径返回
 	sass-loader -> 用来处理sass/scss文件 
 	node-sass -> sass-loader依赖
 	less
 	less-loader-> 用来处理less文件
+	webpack-dev-server -> 在本地开启一个简单的静态服务来进行开发。
--- day20-webpack4.x/webpack.config.js
+++ day20-webpack4.x/webpack.config.js
@@ -1,33 +1,48 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.resolve(__dirname, './src/index.html'),
-    filename: "index.html"
+    template: path.resolve(__dirname, './src/index.html'), // 配置文件模版
+    filename: "index.html" // 配置输出文件名和路径
 });
 
 module.exports = {
     mode: "development",
     // entry:"",
-    entry: './src/index.js',// 默认入口
-    // output:"",
-    plugins: [
-        htmlPlugin
-    ],
+    entry: './src/index.js', // 默认入口
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js' // 默认./dist/main.js
+    },
     module: {
         rules: [{
-            test: /\.js|jsx$/,
+            resource: { // resource 的匹配条件
+                test: /\.js|jsx$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ] // 指定哪些路径下的文件需要经过 loader 处理 
+            },
+            // 如果要使用 issuer 匹配，便是 issuer: { test: ... }
+            exclude: /node_modules/, // 指定那些路径下的文件不需要经过loader处理
             use: "babel-loader",
-            exclude: /node_modules/
         }, {
-            test: /\.css$/,
-            use: [
+            test: /\.css$/, // 1. 匹配条件
+            use: [ // 2. 匹配规则后应用结果  //1跟2是loader的匹配规则中最关键的因素
                 'style-loader',
                 'css-loader'
-            ]
+            ],
+            include: [
+                path.resolve(__dirname, 'src'),
+            ] // 一个object即一条规则
         }, {
             test: /\.jpg|png|gif|bmp$/,
-            use: 'url-loader'
+            use: [{
+                loader: 'file-loader',
+                options: {}
+            }],
+
         }, {
             test: /\.ttf|woff|woff2|eot|svg$/,
             use: 'url-loader' // 打包处理 字体文件的loader
@@ -36,13 +51,47 @@ module.exports = {
             use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:8]', 'sass-loader'] //打包处理scss文件的loader
         }, {
             test: /\.less$/,
-            use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:8]', 'less-loader'] //打包处理scss文件的loader
+            // 导出.css文件的写法
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的loader
+                use: ['css-loader?modules&localIdentName=[path][name]-[local]-[hash:8]', 'less-loader'] //打包处理scss文件的loader 
+            })
+            // 不需要额外输出.css文件的写法什么都不要时候的写法
+            // use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:8]', 'less-loader'] //打包处理scss文件的loader
         }]
     },
+    // 代码模块路径解析的配置
     resolve: {
+        modules: [ // 这种配置在某种程度上可以简化模块的查找，提升构建速度。
+            "node_modules", // 如果有一些类库是放在一些奇怪的地方的，你可以添加自定义的路径或者目录
+            path.resolve(__dirname, 'node_modules') // 指定当前目录下的 node_modules 优先查找
+        ],
         extensions: ['.js', '.jsx', '.json'], //表示这几个文件的后缀名可省略不写，
         alias: { // 表示别名
             '@': path.resolve(__dirname, './src') //这样，@就表示项目根目录中src这一层路径
-        }
-    }
+        },
+        // 配置 target === "web" 或者 target === "webworker" 时 mainFields 默认值是：
+        //   mainFields: ['browser', 'module', 'main'],
+
+        // target 的值为其他时，mainFields 默认值为：
+        mainFields: ["module", "main"],
+        mainFiles: ['index'], // 你可以添加其他默认使用的文件名,通常情况下我们也无须修改这个配置，index.js 基本就是约定俗成的了。
+        resolveLoader: { //用于配置解析 loader 时的 resolve 配置，原本 resolve 的配置项在这个字段下基本都有。
+            extensions: ['.js', '.json'],
+            mainFields: ['loader', 'main'],
+        } //我们一般遵从标准的使用方式，使用默认配置，然后把 loader 安装在项目根路径下的 node_modules 下就可以了。
+    },
+    plugins: [
+        htmlPlugin,
+        new UglifyPlugin(),
+        new ExtractTextPlugin('[name][hash].css'),
+        // 使用 uglifyjs-webpack-plugin 来压缩 JS 代码
+        // 默认已经使用了 JS 代码压缩的插件
+        // mode: production 时会自动压缩，
+        new HtmlWebPackPlugin.DefinePlugin({
+            PRODUCTION:JSON.stringify(true), // const PRODUCTION = true;
+            VERSION:JSON.stringify('5fa3b9'), // const VERSION = '5fa3b9';
+        })
+    ],
 }
